2882024b7b818a214b6516efe403689b6beb9a0e
Joyce Rocha <joycerocha.j@gmail.com>
2024-07-21 11:32:45 -0400

    Ajustes

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ document
     event.preventDefault();
     alert("Formulário enviado com sucesso!");
   });
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".carousel-item");
+  if (index >= slides.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide = index;
+  }
+  const offset = -currentSlide * 100;
+  document.querySelector(
+    ".carousel-inner"
+  ).style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  showSlide(currentSlide);
+  document.querySelector(".next").addEventListener("click", nextSlide);
+  document.querySelector(".prev").addEventListener("click", prevSlide);
+});
